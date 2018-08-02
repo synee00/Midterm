686d507812f6a4c284665da5f42989580d616afe
Jasmine Allen <synee00@gmail.com>
2018-08-02 10:49:23 -0400

    initial commit

---
+++ src/Story.java
@@ -0,0 +1,14 @@
+
+public class Story {
+
+	public static void main(String[] args) {
+
+		System.out.println("Once upon a time in the mystical land of Shangrila there "
+				+ "dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
+		
+		//Start story here
+		
+		
+	}
+
+}
